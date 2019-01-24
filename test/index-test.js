20d18646a69bd4bf16466d03cb2905d3da94070a
Jason Velez <jvelez20@yahoo.com>
2019-01-24 16:14:05 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 /*global describe, it */
 
 describe('Objects', function() {
-  it('defines a `recipes` object', function() {
+  console.log('defines a `recipes` object', function() {
     expect(typeof recipes).toEqual('object')
   })
 
